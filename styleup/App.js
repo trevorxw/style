618c1e92562b6ae961d8e130904a3c94b5543e5f
Trevor Wong <trevorwongalt@gmail.com>
2024-03-15 16:45:36 -0700

    User authentication complete
    Added clerk useWarmUpBrowser, as well as changes to LoginScreen and App to take advantage.

--- styleup/App.js
+++ styleup/App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './apps/screens/LoginScreen';
-import { Clerk, ClerkProvider } from '@clerk/clerk-expo';
+import { Clerk, ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
 
 export default function App() {
   return (
     <ClerkProvider publishableKey='pk_test_bGlnaHQtZWFnbGUtNTguY2xlcmsuYWNjb3VudHMuZGV2JA'>
       <View className="flex-1 bg-white">
         <StatusBar style="auto" />
-        <LoginScreen/>
+        <SignedIn>
+          <Text className="mt-10">You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+          <LoginScreen/>
+        </SignedOut>
       </View>
     </ClerkProvider>
   );
