9a62a9458179f139f98067eb0dca504def65079f
Trevor Wong <trevorwongalt@gmail.com>
2024-03-15 18:49:52 -0700

    Firebase setup

--- styleup/apps/screens/AddPostScreen.jsx
+++ styleup/apps/screens/AddPostScreen.jsx
@@ -1,10 +1,29 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { app } from '../../firebaseConfig';
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function AddPostScreen() {
-  return (
-    <View>
-      <Text>AddPostScreen</Text>
-    </View>
-  )
+
+    // Initialize Cloud Firestore and get a reference to the service
+    const db = getFirestore(app);
+    const [categoryList,setCategoryList]=useState([]);
+    useEffect(()=>{
+        getCategoryList();
+    },[])
+
+    const getCategoryList=async()=>{
+        const querySnapshot=await getDocs(collection(db, 'Category'));
+
+        querySnapshot.forEach((doc)=>{
+            console.log("Docs:", doc.data());
+            setCategoryList(categoryList=>[...categoryList,doc.data()])
+        })
+    }
+    return (
+        <View>
+            <Text>AddPostScreen</Text>
+        </View>
+    )
 }
