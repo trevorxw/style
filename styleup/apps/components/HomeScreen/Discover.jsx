b259d4ca9731fcb81054fa2932d65538a8fb7542
Trevor Wong <trevorwongalt@gmail.com>
2024-03-26 01:26:31 -0700

    Realized design doesn't match screen resolution. So frustrating

--- styleup/apps/components/HomeScreen/Discover.jsx
+++ styleup/apps/components/HomeScreen/Discover.jsx
@@ -1,74 +1,101 @@
-import { View, Text, StyleSheet, Button, Image, ActivityIndicator } from 'react-native';
-import Swiper from 'react-native-deck-swiper';
-import React from 'react';
-import Header from './Header';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Button,
+    Image,
+    ActivityIndicator,
+    Dimensions,
+} from "react-native";
+import Swiper from "react-native-deck-swiper";
+import React from "react";
+import Header from "./Header";
+import Save from "./Save";
+
+const screenHeight = Dimensions.get("window").height;
 
 export default function Discover({ latestCards }) {
-  console.log({ latestCards });
-
-  if (!latestCards || latestCards.length === 0) {
-    return (
-      <View style={styles.container}>
-        <ActivityIndicator color="#4FD0E9" size="large" />
-      </View>
-    );
-  }
-
-  return (
-    <Swiper
-      className='h-full w-full object-cover flex'
-      cards={latestCards}
-      renderCard={(card) => {
-        console.log(card);
+    if (!latestCards || latestCards.length === 0) {
         return (
-          <View style={styles.card}>
-            <Image
-              source={{ uri: card.image }}
-              style={styles.image}
-            />
-            {/* <Text style={styles.text}>hi</Text> */}
-          </View>
+            <View style={styles.container}>
+                <ActivityIndicator color="#ffff" size="large" />
+            </View>
         );
-      }}
-      onSwiped={(cardIndex) => { console.log(cardIndex); }}
-      onSwipedAll={() => { console.log('onSwipedAll'); }}
-      cardIndex={0}
-      backgroundColor={'#4FD0E9'}
-      stackSize={3}
-      verticalSwipe={false}
-    >
-      <Header />
-      <Button
-        onPress={() => { console.log('oulala'); }}
-        title="Press me">
-        You can press me
-      </Button>
-    </Swiper>
-  );
+    }
+
+    return (
+        <View style={styles.outerContainer}>
+            <Swiper
+                cards={latestCards}
+                renderCard={(card) => {
+                    console.log(card);
+                    return (
+                        <View
+                            style={[
+                                styles.card,
+                                { height: screenHeight * 1.5 },
+                            ]}
+                        >
+                            <Image
+                                source={{ uri: card.image }}
+                                style={styles.image}
+                            />
+                            {/* <Text style={styles.text}>hi</Text> */}
+                        </View>
+                    );
+                }}
+                onSwiped={(cardIndex) => {
+                    console.log(cardIndex);
+                }}
+                onSwipedAll={() => {
+                    console.log("onSwipedAll");
+                }}
+                cardIndex={0}
+                backgroundColor={"#4FD0E9"}
+                stackSize={3}
+                stackScale={0}
+                stackSeparation={0}
+                verticalSwipe={false}
+                outputRotationRange={["0deg", "0deg", "0deg"]}
+            >
+                {/* <Header style={styles.header}/> */}
+            </Swiper>
+            {/* <Save style={styles.saveButton}/> */}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  card: {
-    flex: 1,
-    borderRadius: 4,
-    borderWidth: 2,
-    borderColor: "#E8E8E8",
-    justifyContent: "center",
-    backgroundColor: "white"
-  },
-  image: {
-    width: '100%',
-    height: 400,
-    resizeMode: 'cover'
-  },
-  text: {
-    textAlign: "center",
-    fontSize: 40,
-    backgroundColor: "transparent"
-  }
+    outerContainer: {
+        flex: 1,
+        justifyContent: "flex-start",
+        backgroundColor: "#FFFF",
+        height: 110,
+    },
+    card: {
+        flex: 1,
+        borderRadius: 4,
+        borderWidth: 2,
+        borderColor: "#E8E8E8",
+        justifyContent: "center",
+        backgroundColor: "white",
+        marginBottom:40,
+    },
+    image: {
+        flex: 1,
+        height: "100%",
+        width: "100%",
+        resizeMode: "cover",
+    },
+    saveButton: {
+        position: "absolute",
+        bottom: 40,
+        right: 40,
+    },
+    header: {
+        flex: 1,
+        position: "absolute",
+        bottom: 40,
+        right: 40,
+    },
 });
