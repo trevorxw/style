80f240f8df88ca54f5675e378d8d87d9bbbb1664
Trevor Wong <trevorwongalt@gmail.com>
2024-04-11 22:28:48 +0200

    Like Button Works, Share Button increments share, share functionality doesn't work

--- styleup/apps/components/HomeScreen/Like.jsx
+++ styleup/apps/components/HomeScreen/Like.jsx
@@ -5,7 +5,6 @@ import { getFirestore, doc, updateDoc, increment } from "firebase/firestore";
 import { app } from "../../../firebaseConfig";
 
 export default function Like({ card }) {
-    console.log(card);
     const db = getFirestore(app);
     // Assuming card.id is the document ID. Adjust according to your data structure.
     const likesRef = doc(db, "UserPost", card.id);
@@ -17,20 +16,21 @@ export default function Like({ card }) {
         await updateDoc(likesRef, {
             likes: increment(1),
         });
-        // Consider calling changeColor() here after successfully updating to ensure state consistency
-        changeColor();
     };
-
-    // Function to handle press action
-    const changeColor = () => {
-        setIsLiked(!isLiked); // Toggle the liked state
+    const decrementLike = async () => {
+        await updateDoc(likesRef, {
+            likes: increment(-1),
+        });
     };
 
     return (
         <View>
             <TouchableOpacity
                 onPress={() => {
-                    incrementLike(); // Correctly call the functions when pressed
+                    {
+                        isLiked ? decrementLike() : incrementLike();
+                        setIsLiked(!isLiked);
+                    } // Correctly call the functions when pressed
                 }}
                 style={styles.buttons}
             >
@@ -40,7 +40,6 @@ export default function Like({ card }) {
                     color={isLiked ? "red" : "white"}
                 />
             </TouchableOpacity>
-            {/* Assuming card.likes is a number. Adjust if it's stored differently. */}
             <Text style={styles.buttonText}>{card.likes}</Text>
         </View>
     );
--- styleup/apps/components/HomeScreen/Share.jsx
+++ styleup/apps/components/HomeScreen/Share.jsx
@@ -1,11 +1,34 @@
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
-import React from "react";
+import React, { useState } from "react";
+import { getFirestore, doc, updateDoc, increment } from "firebase/firestore";
+import { app } from "../../../firebaseConfig";
 
 export default function Share({ card }) {
+    const db = getFirestore(app);
+    // Assuming card.id is the document ID. Adjust according to your data structure.
+    const shareRef = doc(db, "UserPost", card.id);
+
+    // State to track if the icon is liked
+    const [isShared, setIsShared] = useState(false);
+
+    const incrementShare = async () => {
+        await updateDoc(shareRef, {
+            shares: increment(1),
+        });
+    };
+
     return (
         <View>
-            <TouchableOpacity style={styles.buttons}>
+            <TouchableOpacity
+                onPress={() => {
+                    if (!isShared) {
+                        incrementShare();
+                        setIsShared(true);
+                    }
+                }}
+                style={styles.buttons}
+            >
                 <FontAwesome5 name="share" size={35} color="white" />
             </TouchableOpacity>
             <Text style={styles.buttonText}>{card.shares}</Text>
