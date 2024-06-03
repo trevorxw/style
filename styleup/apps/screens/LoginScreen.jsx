3616171efc2ec140087a758fbea46612341ae36d
Trevor Wong <trevorwongalt@gmail.com>
2024-06-02 18:59:37 -0700

    Change app name

--- styleup/apps/screens/LoginScreen.jsx
+++ styleup/apps/screens/LoginScreen.jsx
@@ -33,7 +33,7 @@ export default function LoginScreen() {
             className='w-full h-[400px] object-cover'
       />
       <View className="p-8 bg-white mt-[-20px] rounded-t-3xl shadow-md">
-        <Text className="text-[30px] font-bold">StyleUp</Text>
+        <Text className="text-[30px] font-bold">Find</Text>
         <Text className="text-[18px] text-slate-500 mt-6">Upgrade your style with your own tailored marketplace!</Text>
         <TouchableOpacity onPress={onPress} className="p-4 bg-blue-500 rounded-full mt-20">
             <Text className="text-white text-center text-[18px]">Get Started</Text>
