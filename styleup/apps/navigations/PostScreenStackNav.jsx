8ba1a8fe5e0cfa89755b7a2251e8afa5bfb0f443
Trevor Wong <trevorwongalt@gmail.com>
2024-06-25 15:47:45 -0700

    Removing deprecated CameraScreen and PreviewScreen

--- styleup/apps/navigations/PostScreenStackNav.jsx
+++ styleup/apps/navigations/PostScreenStackNav.jsx
@@ -2,8 +2,6 @@ import { View, Text } from "react-native";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import AddPostScreen from "../screens/Post/AddPostScreen";
-import CameraScreen from "../screens/Post/CameraScreen";
-import PreviewScreen from "../screens/Post/PreviewScreen";
 
 const Stack = createStackNavigator();
 
@@ -14,17 +12,6 @@ export default function PostScreenStackNav() {
                 name="post1"
                 component={AddPostScreen}
                 options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="camera"
-                component={CameraScreen}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="preview"
-                component={PreviewScreen}
-                options={{ headerShown: false }}
-                initialParams={{ image: null }}
             />
         </Stack.Navigator>
     );
